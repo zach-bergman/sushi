3ceaab5829b057ebb4bdcdd286f08451565fdf13
ziceberg <bzach634@gmail.com>
2019-08-09 18:17:16 -0600

    lots of styling

---
+++ js/script.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+    // ripples
+    $("#header, .info").ripples({
+        dropRadius: 25,
+        perturbance: 0.6,
+    });
+    // magnific popup
+    $('.parent-container').magnificPopup({
+        delegate: 'a', // child items selector, by clicking on it popup will open
+        type: 'image',
+        gallery: {
+            enabled: true
+        }
+        // other options
+      });
+});
